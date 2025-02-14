886e50a922a74fadd5c91b0737fe35848795919b
sixstar81 <sixstar6@dbgroup.co.kr>
2025-02-14 15:20:08 +0900

    two

--- src/main/java/com/sk/etl/EtlApplication.java
+++ src/main/java/com/sk/etl/EtlApplication.java
@@ -1,13 +1,26 @@
 package com.sk.etl;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+//@PropertySources({
+//		@PropertySource("classpath:config/application.db-${spring.profiles.active}.properties")
+//})
 public class EtlApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EtlApplication.class, args);
 	}
 
+	//초기화 로직
+//	@Bean
+//	public CommandLineRunner runner(UserRepository userRepository) {
+//		return (args) -> {
+//			userRepository.save(new User("홍길동", "hong@naver.com", "1111"));
+//		};
+//	}
 }
